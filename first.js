9e0d28a71eaccb06f4fcfdceb8f0ae5c82a64cc1
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-07-24 15:02:01 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,8 @@
+console.log('hellow world');
+console.log(23);
+console.log({name:'shivang shrivastava',age:23});
+console.log(true,false);
+const myName='shivang';//this is variable whare  myName is in camel case ;
+const $myAge=20;
+const myidentification={name:myName,age:$myAge};
+console.log(myidentification);
