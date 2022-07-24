5868657e7b70e8a8684531e65132232ed06a8202
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-07-24 15:02:29 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,21 @@
+/*function description(firstname,lastname){
+    const output=`my name is ${firstname} ${lastname}.`;
+    return output;
+}
+console.log(description("shivang","shrivastava"));
+console.log(description("ravikant","shrivastava"));
+
+functonone();
+function functonone(){
+    console.log("This is function one")
+} 
+//functonone();
+*/
+
+// function expression better to write code.
+
+//functiontwo(); this shows error
+const functiontwo=function(){
+    console.log("Hi i am function two");
+}
+functiontwo();
