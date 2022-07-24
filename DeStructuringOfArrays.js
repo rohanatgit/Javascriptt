6746d66c7dd515ddcaf983b005b1d59b66fc36f4
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-07-24 14:58:41 +0530

    Add files via upload
    DeStructuring

---
+++ DeStructuringOfArrays.js
@@ -0,0 +1,21 @@
+/*const names=['shivang','suyash','saurabh','rohit','rohan'];
+console.log(names[1],names[2]);
+const [,nameIndexone, , ,nameIndextwo]=names;
+console.log(nameIndexone,nameIndextwo)
+
+const arr=[1,2,[3,4]];
+let [i, ,[j,k]]=arr;
+console.log(i, j,k);
+[j,i]=[i,j];
+console.log(i,j);*/
+
+//const str1='GeeksforGeeks';
+//const str2=str1.slice(2,5);
+//console.log(str2);
+
+for(var i=0;i<3;i++){
+    setTimeout(()=>console.log(i),1);
+}
+for(var i=0;i<3;i++){
+    setTimeout(()=>console.log(i),1);
+}
